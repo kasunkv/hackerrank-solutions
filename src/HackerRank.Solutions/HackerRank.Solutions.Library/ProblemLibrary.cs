c9be7a31627c9173c88c337378a744230f988ca5
Kasun Kodagoda <kvkrusl@gmail.com>
2023-01-17 23:56:37 +0530

    Added the solve me first problem

---
+++ src/HackerRank.Solutions/HackerRank.Solutions.Library/ProblemLibrary.cs
@@ -0,0 +1,23 @@
+﻿using HackerRank.Solutions.Contracts;
+using HackerRank.Solutions.Library.Warnup;
+using HackerRank.Solutions.Shared;
+
+namespace HackerRank.Solutions.Library
+{
+    public class ProblemLibrary
+    {
+        public Dictionary<HackerRankProblem, ISolution> Problems { get; private set; }
+
+        public List<string> ProblemTitles => Problems.Values.Select(p => p.Title).ToList();
+
+        public List<HackerRankProblem> ProblemIds => Problems.Keys.ToList();
+
+        public ProblemLibrary()
+        {
+            Problems = new Dictionary<HackerRankProblem, ISolution>
+            {
+                { HackerRankProblem.SolveMeFirst, new SolveMeFirst() },
+            };
+        }
+    }
+}
---
+++ src/HackerRank.Solutions/HackerRank.Solutions.Library/Warnup/SolveMeFirst.cs
@@ -0,0 +1,28 @@
+﻿using HackerRank.Solutions.Contracts;
+
+namespace HackerRank.Solutions.Library.Warnup
+{
+    public class SolveMeFirst : BaseSolution, ISolution
+    {
+        public string Title => "Solve Me First";
+
+        public Shared.HackerRankProblem Id => Shared.HackerRankProblem.SolveMeFirst;
+
+
+        public int Answer(int a, int b)
+        {
+            return a + b;
+        }
+
+
+        public void Execute()
+        {
+            int input1 = 2;
+            int input2 = 3;
+
+            var result = Answer(input1, input2);
+
+            PrintAnswer(Title, result.ToString());
+        }
+    }
+}
